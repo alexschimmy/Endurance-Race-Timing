d03d66bd9903d76a09afd9999f0caad2a64bb70c
alexschimmy <alexschimmy@gmail.com>
2015-02-24 18:24:00 -0600

    Update MeetDao.java

--- src/com/enduranceRaceTiming/domain/meet/MeetDao.java
+++ src/com/enduranceRaceTiming/domain/meet/MeetDao.java
@@ -2,9 +2,11 @@ package com.enduranceRaceTiming.domain.meet;
 
 import java.util.List;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import com.enduranceRaceTiming.controller.events.SearchCriteria;
 import com.enduranceRaceTiming.domain.common.HibernateUtil;
@@ -12,24 +14,37 @@ import com.enduranceRaceTiming.domain.common.HibernateUtil;
 public class MeetDao {
 
 	public Meet get(Integer id) {
-        SessionFactory sf = HibernateUtil.getSessionFactory();
-        Session session = sf.openSession();
- 
-        Meet meet = (Meet) session.get(Meet.class, id);
-        session.close();
-        
-        return meet;
-    }
-	
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = null;
+		Meet meet = null;
+
+		try {
+			tx = session.beginTransaction();
+
+			meet = (Meet) session.get(Meet.class, id);
+
+			tx.commit();
+		} catch (HibernateException he) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			he.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return meet;
+	}
+
 	public List<Meet> search(SearchCriteria searchCriteria) {
 		SessionFactory sf = HibernateUtil.getSessionFactory();
-        Session session = sf.openSession();
-        
+		Session session = sf.openSession();
+
 		Query query = session.getNamedQuery("findMeetsByYear")
 				.setInteger("year", new Integer(searchCriteria.getYear()));
-		
+
 		List<Meet> meets = query.list();
-		
+
 		return meets;
 	}
 }
